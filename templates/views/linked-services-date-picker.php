0eb51efbc8f54e60c6fefde7376dc21eb680f5ae
shabbarcendas <44211820+MuhammadShabbarAbbas@users.noreply.github.com>
2024-12-31 17:39:15 +0500

    fixed target date

--- templates/views/linked-services-date-picker.php
+++ templates/views/linked-services-date-picker.php
@@ -21,7 +21,7 @@ $end_date = $end_date->format('Y-m-d');
     echo OsStepsHelper::get_formatted_extra_step_content($current_step_code, 'before');
     ?>
     <div class="os-dates-w">
-        <?php OsLinkedServicesCalendarHelper::generate_calendar_for_datepicker_step(\LatePoint\Misc\BookingRequest::create_from_booking_model($linked_services_booking), new OsWpDateTime($calendar_start_date), ['timezone_name' => OsTimeHelper::get_timezone_name_from_session(), 'consider_cart_items' => true, 'earliest_possible_booking'=> $booking->start_date, 'latest_possible_booking' => $end_date]); ?>
+        <?php OsLinkedServicesCalendarHelper::generate_calendar_for_datepicker_step(\LatePoint\Misc\BookingRequest::create_from_booking_model($linked_services_booking), new OsWpDateTime($booking->start_date), ['timezone_name' => OsTimeHelper::get_timezone_name_from_session(), 'consider_cart_items' => true, 'earliest_possible_booking'=> $booking->start_date, 'latest_possible_booking' => $end_date]); ?>
     </div>
     <div class="linked-service-time-selector-w <?php echo OsStepsHelper::hide_unavailable_slots() ? 'hide-not-available-slots' : ''; ?> <?php echo 'time-system-'.OsTimeHelper::get_time_system(); ?> <?php echo (OsSettingsHelper::is_on('show_booking_end_time')) ? 'with-end-time' : 'without-end-time'; ?> style-<?php echo OsStepsHelper::get_time_pick_style(); ?>">
         <div class="times-header">
